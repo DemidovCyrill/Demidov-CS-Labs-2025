c3daca6b321dcfa668f84fd607d975082feff762
DemidovCyrill <demidovcyrill@yandex.ru>
2025-02-25 10:16:10 +0300

    Logic programm

--- Planet/Planet.cpp
+++ Planet/Planet.cpp
@@ -2,3 +2,173 @@
 #include <cstring>
 #include <algorithm>
 #include <fstream>
+
+Planet::Planet() : name(nullptr), diameter(0), hasLife(false), satellites(0) {
+}
+
+Planet::Planet(const char* name, long long diameter, bool hasLife, int satellites)
+    : diameter(diameter), hasLife(hasLife), satellites(satellites) {
+    this->name = new char[strlen(name) + 1];
+    strcpy(this->name, name);
+}
+
+// Planet::Planet(const Planet& other) : name(nullptr) {
+//     copyFrom(other);
+// }
+
+Planet::~Planet() {
+    cleanup();
+}
+
+// bool Planet::operator<(const Planet& other) const {
+//     return diameter < other.diameter;
+// }
+
+// bool Planet::operator==(const Planet& other) const {
+//     return diameter == other.diameter;
+// }
+
+std::ostream& operator<<(std::ostream& os, const Planet& planet) {
+    os << planet.name << " " << planet.diameter << " " << planet.hasLife << " " << planet.satellites;
+    return os;
+}
+
+std::istream& operator>>(std::istream& is, Planet& planet) {
+    char name[256];
+    is >> name;
+    delete[] planet.name;
+    planet.name = new char[strlen(name) + 1];
+    strcpy(planet.name, name);
+
+    is >> planet.diameter >> planet.satellites >> planet.hasLife;
+    return is;
+}
+
+void Planet::readFromFile(const char* filename, Planet*& planets, int& size) {
+    std::ifstream file(filename);
+    if (!file) {
+        std::cout << "!!!ОШИБКА!!!" << std::endl << "Не удалось открыть файл!" << std::endl;
+        return;
+    }
+
+    file >> size;
+    file.ignore();
+    std::cout << "УСПЕШНО!";
+    std::cout << std::endl << "Хотите проверить полученную информацию? (y/n): ";
+    char flag;
+    char error_flag = false;
+    std::cin >> flag;
+    if (flag == 'y') {
+        std::cout << "Прочитан размер: " << size << std::endl;
+    }
+
+    if (planets != nullptr) {
+        delete[] planets;
+        planets = nullptr;
+    }
+
+    planets = new Planet[size];
+
+    for (int i = 0; i < size; i++) {
+        char name[256];
+        long long diam;
+        int life;
+        int sat;
+
+        if (file >> name >> diam >> life >> sat) {
+            if (flag == 'y') {
+                std::cout << "Попытка создания планеты " << i << ": "
+                      << name << " " << diam << " "
+                      << sat << " " << life << std::endl;
+            }
+
+            planets[i] = Planet(name, diam, sat, life != 0);
+            if (flag == 'y') {
+                std::cout << "Планета " << i << " успешно создана" << std::endl;
+            } else {
+                std::cout << "Ошибка при чтении планеты " << i << std::endl;
+                error_flag = true;
+                break;
+            }
+        }
+    }
+    if (error_flag & (flag != 'y')) {std::cout << "Была некая ошибка при чтении планеты!";}
+    file.close();
+}
+
+void Planet::writeToFile(const char* filename, const Planet* planets, int size) {
+    std::ofstream file(filename);
+    if (!file) {
+        std::cerr << "Не удалось открыть файл для записи." << std::endl;
+        return;
+    }
+
+    file << size << std::endl;
+    for (int i = 0; i < size; ++i) {
+        file << planets[i] << std::endl;
+    }
+
+    file.close();
+}
+
+void Planet::sortPlanets(Planet* planets, int size) {
+    std::sort(planets, planets + size);
+}
+
+void Planet::addPlanet(Planet*& planets, int& size, const Planet& newPlanet) {
+    Planet* newArray = new Planet[size + 1];
+    for (int i = 0; i < size; ++i) {
+        newArray[i] = planets[i];
+    }
+    newArray[size] = newPlanet;
+    delete[] planets;
+    planets = newArray;
+    ++size;
+}
+
+void Planet::removePlanet(Planet*& planets, int& size, int index) {
+    if (index < 0 || index >= size) {
+        std::cerr << "Неверный индекс для удаления." << std::endl;
+        return;
+    }
+
+    Planet* newArray = new Planet[size - 1];
+    int j = 0;
+    for (int i = 0; i < size; ++i) {
+        if (i != index) {
+            newArray[j++] = planets[i];
+        }
+    }
+    delete[] planets;
+    planets = newArray;
+    --size;
+}
+
+void Planet::editPlanet(Planet& planet) {
+    std::cout << "Введите новые данные для планеты:" << std::endl;
+    std::cin >> planet;
+}
+
+void Planet::displayPlanets(const Planet* planets, int size) {
+    for (int i = 0; i < size; ++i) {
+        std::cout << planets[i] << std::endl;
+    }
+}
+
+
+void Planet::copyFrom(const Planet& other) {
+    if (other.name) {
+        name = new char[strlen(other.name) + 1];
+        strcpy(name, other.name);
+    } else {
+        name = nullptr;
+    }
+    diameter = other.diameter;
+    satellites = other.satellites;
+    hasLife = other.hasLife;
+}
+
+void Planet::cleanup() {
+    delete[] name;
+    name = nullptr;
+}
--- main.cpp
+++ main.cpp
@@ -1,33 +1,168 @@
 #include "Planet/Planet.h"
+#include "Ticket/Ticket.h"
 #include <iostream>
 #include <fstream>
 #include <limits>
+#include <typeinfo>
 
 char menu(){
-    std::cout << " ========================= ГЛАВНОЕ МЕНЮ =========================\n";
+    std::cout << " ========================= ГЛАВНОЕ МЕНЮ ====================================\n";
+    std::cout << "|| 1 - Работа с планетами\t\t\t || 3 - Запуск тестов     ||" << std::endl;
+    std::cout << "|| 2 - Работа с железнодорожными билетами\t || Любая клавиша - ВЫХОД ||" << std::endl;
+    std::cout << " ===========================================================================\n";
+    std::cout << "Для выбора операции введите цифру от 1 до 3" << std::endl;
+    char answer;
+    std::cin >> answer;
+    std::cout << std::endl;
+    std::cout << std::endl;
+    return answer;
+}
+
+char MenuDatabase(){
+    std::cout << " ========================= Работа с БД ==========================\n";
     std::cout << "|| 1 - чтение БД из файла\t || 5 - удаление объекта из БД ||" << std::endl;
     std::cout << "|| 2 - запись БД в файл\t\t || 6 - редактирование БД      ||" << std::endl;
     std::cout << "|| 3 - сортировка БД\t\t || 7 - вывод БД на экран      ||" << std::endl;
-    std::cout << "|| 4 - добавление объекта в БД\t || Любая клавиша - ВЫХОД      ||" << std::endl;
+    std::cout << "|| 4 - добавление объекта в БД\t || Любая клавиша - НАЗАД      ||" << std::endl;
     std::cout << " ================================================================\n";
     std::cout << "Для выбора операции введите цифру от 1 до 7" << std::endl;
     char answer;
     std::cin >> answer;
+    std::cout << std::endl;
+    std::cout << std::endl;
     return answer;
 }
+
+void clearInputBuffer() {
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+bool Equal(char m1[256], char m2[256]){
+    for (int i = 0;  m1[i] != '\0' || m2[i] != '\0'; ++i){
+        if (m1[i] != m2[i]){
+            return false;
+        }
+    }
+    return true;
+}
+
+template<typename T>
+void DatabaseFromFile(T* items, int size){
+    char filename[256];
+    char flag_0[256]{};
+    flag_0[0] = '0';
+    clearInputBuffer();
+    std::cout << "Введите имя файла для чтения или цифру «0» для осведомления с необходимой структурой файла: ";
+    std::cin >> filename;
+    if (!Equal(filename, flag_0)) {
+        //T::readFromFile(filename, items, size);
+    } else if (typeid(T) ==typeid(Planet)){
+        std::cout << std::endl << "1-я строка: кол-во планет;";
+        std::cout << std::endl << "Далее в каждой строчке по одному объекту планеты со следующими характеристиками через пробел:";
+        std::cout << std::endl << "1)Название 2)Диаметр 3)Жизнь 4)Спутники" << std::endl;
+        std::cout << "\nА теперь введите имя файла для чтения: ";
+        std::cin >> filename;
+        //T::readFromFile(filename, items, size);
+    } else {
+        std::cout << std::endl << "1-я строка: кол-во билетов;";
+        std::cout << std::endl << "Далее в каждой строчке по одному объекту планеты со следующими характеристиками через пробел:";
+        std::cout << std::endl << "1)Рейс(Куда) 2)№Вагона 3)№Места 4)Цена" << std::endl;
+        std::cout << "\nА теперь введите имя файла для чтения: ";
+        std::cin >> filename;
+        //T::readFromFile(filename, items, size);
+    }
+}
+
+template<typename T>
+void DatabaseToFile(T* items, int size){
+    char filename[256];
+    std::cout << "Введите имя файла для записи: ";
+    std::cin >> filename;
+    //T::writeToFile(filename, items, size);
+}
+
+template<typename T>
+void SortDatabase(){
+
+}
+
+template<typename T>
+void AddToDatabase(){
+
+}
+
+template<typename T>
+void DelFromDatabase(){
+
+}
+
+template<typename T>
+void EditDatabase(){
+
+}
+
+template<typename T>
+void PrintDatabase(){
+
+}
+
+
+template<typename T>
+void RunDatabase(const std::string& type) {
+    T* items = nullptr;
+    int size = 0;
+
+    bool exit_run_database = true;
+    while(exit_run_database){
+        switch (MenuDatabase()){
+            case '1':
+                DatabaseFromFile<T>(items, size);
+                break;
+            case '2':
+                DatabaseToFile<T>(items, size);
+                break;
+            case '3':
+                SortDatabase<T>();
+                break;
+            case '4':
+                AddToDatabase<T>();
+                break;
+            case '5':
+                DelFromDatabase<T>();
+                break;
+            case '6':
+                EditDatabase<T>();
+                break;
+            case '7':
+                PrintDatabase<T>();
+                break;
+            default:
+                exit_run_database = false;
+                break;
+        }
+    }
+}
+
 
 int main(){
     bool exit_programm = true;
     while(exit_programm){
         switch (menu())
         {
-        case '1':
-            break;
-
-        default:
-        std::cout << "Спасибо за работу, до свидания!" << std::endl;
-        exit_programm = false;
-            break;
+            case '1':
+                RunDatabase<Planet>("планет");
+                break;
+            case '2':
+                RunDatabase<Ticket>("билет");
+                break;
+            case '3':
+                break;
+            default:
+            std::cout << "Спасибо за работу, до свидания!" << std::endl;
+            exit_programm = false;
+                break;
         }
+     clearInputBuffer();
     }
 }
