ea9fe95dcfbbece302adaee47eb188a3f0d6f137
DemidovCyrill <demidovcyrill@yandex.ru>
2025-03-18 15:10:35 +0300

    skibidi

--- MySet/MySet.h
+++ MySet/MySet.h
@@ -10,13 +10,12 @@ private:
     int q_find(const T& element, int left, int right) const;
 
 public:
-    using MyVector<T>::MyVector;  // Наследуем конструкторы
+    using MyVector<T>::MyVector;
 
     bool is_element(const T& element) const;
     void add_element(const T& element);
     void delete_element(const T& element);
 
-    // Перегрузка операторов
     MySet& operator+=(const MySet& other);
     MySet& operator-=(const MySet& other);
     MySet& operator*=(const MySet& other);
@@ -35,7 +34,6 @@ public:
 
     friend MySet operator*(const MySet& a, const MySet& b) {
         MySet result;
-        // Правильная реализация пересечения множеств
         for (size_t i = 0; i < a.size; ++i) {
             if (b.is_element(a.pdata[i])) {
                 result.add_element(a.pdata[i]);
@@ -53,6 +51,72 @@ public:
     }
 };
 
-// #include "MySet.сpp"
+///////////////////////
 
-#endif // MYSET_H
+template <typename T>
+bool MySet<T>::is_element(const T& element) const {
+    return q_find(element, 0, this -> size - 1) != -1;
+}
+
+template <typename T>
+void MySet<T>::add_element(const T& element) {
+    if (!is_element(element)) {
+        MyVector<T>::add_element(element);
+        this -> sort();
+    }
+}
+
+template <typename T>
+void MySet<T>::delete_element(const T& element) {
+    int index = this -> find(element);
+    if (index != -1) {
+        MyVector<T>::delete_element(index);
+    }
+}
+
+template <typename T>
+MySet<T>& MySet<T>::operator+=(const MySet& other) {
+    for (size_t i = 0; i < other.size; ++i) {
+        add_element(other.pdata[i]);
+    }
+    return *this;
+}
+
+template <typename T>
+MySet<T>& MySet<T>::operator-=(const MySet& other) {
+    for (size_t i = 0; i < other.size; ++i) {
+        delete_element(other.pdata[i]);
+    }
+    return *this;
+}
+
+template <typename T>
+MySet<T>& MySet<T>::operator*=(const MySet& other) {
+    for (size_t i = 0; i < this->size; ++i) {
+        if (!other.is_element(this->pdata[i])) {
+            delete_element(this->pdata[i]);
+            --i;
+        }
+    }
+    return *this;
+}
+
+template <typename T>
+int MySet<T>::q_find(const T& element, int left, int right) const {
+    if (left > right) return -1;
+    int mid = left + (right - left) / 2;
+    if (this->pdata[mid] == element) return mid;
+    if (this->pdata[mid] > element) return q_find(element, left, mid - 1);
+    return q_find(element, mid + 1, right);
+}
+
+std::ostream& operator<<(std::ostream& os, const MySet<int>& set) {
+    bool first = false;
+    std::cout << "\n{";
+    for (size_t i = 0; i < set.get_size() - 1; ++i)
+        std::cout << set[i] << ", ";
+    std::cout << set[set.get_size()-1] << "}" << std::endl << std::endl;
+    return os;
+}
+
+#endif
--- main.cpp
+++ main.cpp
@@ -9,29 +9,24 @@ int main() {
     set1.add_element(5);
     set1.add_element(6);
 
-    // MySet<int> set2;
-    // set2.add_element(1);
-    // set2.add_element(2);
-    // set2.add_element(3);
-    // set2.add_element(4);
+    MySet<int> set2;
+    set2.add_element(1);
+    set2.add_element(2);
+    set2.add_element(3);
+    set2.add_element(4);
 
-    // MySet<int> union_set = set1 + set2;
-    // MySet<int> intersection_set = set1 * set2;
-    // MySet<int> difference_set = set1 - set2;
+    std::cout << "Элементы множества 1: " << set1;
+    std::cout << "Элементы множества 2: " << set2;
 
-    // std::cout << "Union: ";
-    // for (size_t i = 0; i < union_set.get_size(); ++i)
-    //     std::cout << union_set[i] << " ";
-    // std::cout << std::endl;
-    // std::cout << "Intersection: ";
-    // for (size_t i = 0; i < intersection_set.get_size(); ++i)
-    //     std::cout << intersection_set[i] << " ";
-    // std::cout << std::endl;
+    MySet<int> union_set = set1 + set2;
+    MySet<int> intersection_set = set1 * set2;
+    MySet<int> difference_set = set1 - set2;
 
-    // std::cout << "Difference: ";
-    // for (size_t i = 0; i < difference_set.get_size(); ++i)
-    //     std::cout << difference_set[i] << " ";
-    // std::cout << std::endl;
+    std::cout << "Сумма множеств: " << union_set;
+
+    std::cout << "Пересечение множеств: " << intersection_set;
+
+    std::cout << "Разность множеств: " << difference_set;
 
     return 0;
 }
